3ba192683f94ded6104bc1122de9356bc378b87d
Naywinhtun <n.winhtun.awitd@gmail.com>
2018-10-30 16:44:16 +0630

    first commit

---
+++ blog/resources/assets/js/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+ 
+import Notification from './components/Notification.vue';
+
+ new Vue({
+ 	el:'#app',
+ 	
+ 	components:{ Notification }
+ })
---
+++ blog/resources/assets/js/components/Notification.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="notification">
+     <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+ export default {
+
+   data(){
+      return {
+         message:'Hello World'
+      };
+   }
+ }
+ 
+</script>
+
+<style lang="scss">
+ .notification{
+   background:black;
+ }
+</style>
